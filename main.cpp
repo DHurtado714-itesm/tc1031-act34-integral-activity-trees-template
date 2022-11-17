e6d31ff37fdbd40c405bfb2932a5da3a6116db0f
Daniel Hurtado <daniel_hurtado@Ninja-Hurtado.localdomain>
2022-11-17 10:25:41 -0600

    Heap class created, input files working

--- main.cpp
+++ main.cpp
@@ -1,14 +1,81 @@
 // =================================================================
 //
 // File: main.cpp
-// Author:
-// Date:
+// Author: Daniel Hurtado - A01707774
+// Date: 15 - 11 - 2022
 //
 // =================================================================
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include "header.h"
+#include "heap.h"
+
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
+	
+	ifstream inputFile;
+	ofstream outputFile;
+
+	// Open file and read variables
+	inputFile.open("input0.txt");
+	if (inputFile.fail())
+	{
+		cout << "Error opening file" << endl;
+		return 1;
+	}
+
+	outputFile.open("my_output0.txt");
+	if (outputFile.fail())
+	{
+		cout << "Error opening file" << endl;
+		return 1;
+	}
+
+	int n_list;
+	
+	inputFile >> n_list;
+	
+	Heap<int> Heap(n_list);
+		for (int i = 0; i < n_list; i++)
+		{
+			int number;
+			inputFile >> number;
+			Heap.add(number);
+		}
+
+	cout << Heap.toString() << endl;
+	
 	return 0;
 }
+
+// =================================================================
+// Comentarios
+// =================================================================
+
+/*
+
+Objetivo: 
+	- Desarrolla una solución que, dada un conjunto de pedidos y sus
+	tamaños, calcule el menor número de comparaciones posibles.
+
+Logica:
+	(m + n) - 1
+	- P1 y P2 -> Z1: (20 + 5) - 1 = 24
+	- Z1 y P3 -> Z2: (24 + 8) - 1 = 31
+	- Z2 y P4 -> Z3: (31 + 20) - 1 = 50
+	- Z3 y P5 -> Z4: (50 + 7) - 1 = 56
+	- Z4 y P6 -> Z5: (56 + 4) - 1 = 59
+
+Inputs:
+	- Número de listas (N)
+	- Tamano de cada lista (N)
+
+Outputs:
+	- Número de comparaciones
+
+*/
